cf26e5606be3f1266b1efb9a23dd3ba864f28038
Marcus Walz <mewalz@uwm.edu>
2015-01-30 09:21:36 -0600

    avatar factory more up to code.

--- test/factories/avatar.R
+++ test/factories/avatar.R
@@ -3,21 +3,21 @@
 # E.g. BMI is completly independent of height and weight.
 avatar_random = function () {
   list (
-    "GENDER" = sample(c("M", "F"), 1)
-  , "AGE"    = rnorm(1,50,10)
-  , "RACE"   = sample(c("White", "Black or African American", "Asian"), 1) 
-  , "HEIGHT" = rnorm(1,62,10)
-  , "WEIGHT" = rnorm(1,180,20)
-  , "SMOKER" = sample(c("Y", "N"), 1)
+    "AGE"    = rnorm(1,50,10)
   , "AMI" = sample(c("Y", "N"), 1)
-  , "FLUVASTATIN" = sample(c("Y", "N"), 1)
+  , "CYP2C9" = sample(c("*1/*1", "*1/*2", "*1/*3", "*2/*1","*2/*2", "*2/*3", "*3/*3"), 1)
+  , "DVT" = sample(c("Y", "N"), 1)
   , "ENZYME" = sample(c("Y", "N"), 1)
-  , "CYP2C9" = sample(c("*1/*1", "*1/*2", "*1/*3", "*2/*1","*2/*2", "*2/*3"), 1)
-  , "VKORC1G" = sample(c("G/G", "A/A", "G/A"), 1)
-  , "TINR" = sample(c("low", "N"), 1)
-  , "CONDITION" = "condition_1"
-  , "BMI" = rnorm(1,28,3)
+  , "GENDER" = sample(c("M", "F"), 1)
+  , "HEIGHT" = rnorm(1,62,10)
+  , "RACE"   = sample(c("White", "Black or African American", "Asian", "Unknown"), 1) 
+  , "SMOKER" = sample(c("Y", "N"), 1)
+  , "TINR" = rnorm(1, 2.7, 0.3)
+  , "VKORC1G" = sample(c("G/G", "A/A", "G/A", "Unknown"), 1)
+  , "VKORC1T" = sample(c("C/C", "C/T", "T/T", "Unknown"), 1)
+  , "WEIGHT" = rnorm(1,180,20)
   )
+
 }
 
 # Grabs a random avatar from a dataframe
