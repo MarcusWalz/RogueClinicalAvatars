2f9d9f644723b4f9c7b3e37d1c9c1d83313f09e4
Marcus Walz <mewalz@uwm.edu>
2015-02-19 16:03:05 -0600

    it's doing stuff! It worksgit add prob_table.rb
    '
    
    \'

---
+++ make_avatars.R
@@ -0,0 +1,51 @@
+load("prob_table.Rdata")
+
+tables = table
+
+# loop over the tables
+cat("prob_tables =[")
+for(table_num in 1:length(tables)) {
+  cat("[")
+  table_name = names(tables)[table_num]
+  table = tables[[table_num]]
+
+  cond = colnames(table$Conditions)
+
+  values = colnames(table$Probabilities)
+
+  for(table_row_num in 1:nrow(table$Conditions) ) {
+
+
+  # for field  
+  cat(paste("{ \"for\" => \"", table_name, "\"", sep=""))
+
+  # on_fields
+
+
+  cat(", \"on_fields\" => {")  
+  if(cond != "X0") {
+    for(i in 1:length(cond)) {
+      if(as.character(table$Condition[table_row_num, i]) == "Data Scarcity") {
+        break
+      }
+
+      cat("\"", cond[i], "\" => \"", as.character(table$Conditions[table_row_num,i]), "\" ,", sep="")
+    }
+  }
+  cat("}")
+
+  
+  #probs
+  cat(", \"probs\" => {")   
+  for(i in 1:length(values)) {
+    cat("\"", values[i], "\" => ", as.numeric(as.character((table$Probabilities[table_row_num,i]))), " ,", sep="")
+  }
+  cat("}")
+
+  
+  cat( "}," )
+  }
+  cat("],")
+}
+cat("]\n")
+
