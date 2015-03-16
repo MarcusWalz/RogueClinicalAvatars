8d08575559d7e6576f0d39549d35611aa30c6cd3
Marcus Walz <mewalz@uwm.edu>
2015-03-16 13:14:51 -0500

    parallel simulator

--- queue/iwpc/settings.R
+++
@@ -1,8 +0,0 @@
-sim_settings = list ( protocol = "ahc_clinical"
-                    , initial ="pginitial_IWPC"
-                    , maxDose=15
-                    , days=500
-                    , rseed = 4321
-                    , numReplicates = 1
-                    , per_job = 300
-                    )
--- simulator.R
+++ simulator.R
@@ -1,167 +1,185 @@
 
 #This function takes the following arguments and simulates a course of anticoagulation therapy.
-#avatars: A file includes avatars with assigned initial warfarin doses.
+#avatars: A file includes avatars with assigned initial warfarin dose.
 #protocols: A desired dosing protocol for warfarin maintenance dosing.
 #initialDose: A relevant code for the dosing algorithm used for initial dosing.
 #numDaysToSimulate: A desired number of days for simulation.
-#maxDose: The upper limit of a range of warfarin dose.
+#simulation$max_dose: The upper limit of a range of warfarin dose.
 #numReplicates: Number of replication of a simulation.
-#maxTime: The time interval for getting INR and Dose calculations outputs.
+#max_time: The time interval for getting INR and Dose calculations outputs.
 #rseed: random seeds.
 
 ##################################
 ###    simulation function     ###
 ##################################
-processAvatar = function(avatars, initialDose, protocol, numDaysToSimulate, maxDose, numReplicates, maxTime=24, rseed) {
-###### Note: maxTime arguement is defined based on the adopted PK/PD model. So, since Hamberg PK/PD
-###### model is based on a 24-hr time period, we decided to make argument "maxTime" constant "24".
-###### So, the regular user of the simulator does not need to change it when calling function "processAvatar".
 
-require(deSolve)
+# avatars = df of avatars
+# simulation = list of simulations (length equal to number of rows in avatar) 
+#   or a single base simulation 
+
+library(plyr)
+library(doMC)
 source("hamberg_2007.R")
 source("maintenance_protocols.R")
 source("initial_dose.R")
 source("job.distributor.R")
-    
-    
-    ####### Call function "distribute" in "job.distributor.R"
-    # distribute.out <- distribute(avatars, av_per, av_index, numReplicates)
-    # rseed <- unlist(distribute.out[2][[1]])
-    # avatars <- as.data.frame(distribute.out[1])
-    
-    ####### associate the protocol dose with the avatar group
-    avatars = cbind(avatars, "Protocol" = rep(protocol, nrow(avatars)))
 
-    ####### Call function "initial_dose" in "initial.dosing.library.R" to calculate and assign 
-    ####### an initial dose to each avatar using the selected initial dosing algorithm by
-    ####### the user of the simulator when function "processAvatar" is called.
-    avatars <- initial_dose(avatars, initialDose)
-    
-    numAvatars = nrow(avatars)
-    
-    ID.array <- array(NA, dim=c(numDaysToSimulate, numAvatars))
+my_av = read.table("data_semifinal.txt", sep=" ", header=T)[1:100,]
+my_av$ENZYME <- "Y"
 
-    INR.array = array(NA, dim=c(numDaysToSimulate, numReplicates, numAvatars))
-    INR.check.array = array(0, dim=c(numDaysToSimulate, numReplicates, numAvatars))
-    dose.array = INR.array
-    big_array = c()
-    INRerrorDist = array(rnorm(numDaysToSimulate*numAvatars*numReplicates,0,0.1), dim=c(numDaysToSimulate, numReplicates, numAvatars))   # INR error distribution
-    
-	for(a in 1:numAvatars) {
-    		
-    		for (r in 1:numReplicates) {
-    		    
-    		    # configure the random seed
-    		    if (is.vector(rseed) & numReplicates == 1) {
-    		        if (numReplicates == 1) {
-    		            seed = rseed[a]
-    		        } else {
-    		            seed = rseed[r]
-    		        }
-    		    } else if (is.matrix(rseed)) {
-    		        seed = rseed[a, r]
-		        } else {
-		            stop("Your random seed isn't working like you think it should!")
-		        }
-		        set.seed(seed)
-        	    rnums = round(abs(rnorm(numDaysToSimulate)*100*numDaysToSimulate)) # just a list of random numbers that will be used as seeds for some protocols
+process_avatars = function( avatars
+                          , simulations
+                          , build = T
+                          , initial_seed = 4321
+                          , replicates = 1
+                          , threads = 1 
+                          ) {
+  registerDoMC(threads)
 
-                INRs = array(NA, numDaysToSimulate)
-        		INR.check = array(0, numDaysToSimulate)
-			    Cs_super_out = array(0, dim=c(maxTime*numDaysToSimulate+1, numReplicates))     # keep track of the dose superpositioning for each avatar
-        		doses = INRs
-        		INRtimePoints = c(1:numDaysToSimulate) # this is in days
-		        INRtimePoints = INRtimePoints * 24
-                
-			    # set the initial dose
-			    doses[1] = avatars$InitialDose[a]
+  if(build) {
     
-		        Cs_rows = maxTime*numDaysToSimulate+1
-		        Cs = matrix(0, nrow = Cs_rows, ncol=numDaysToSimulate)
-		        Cs_super = 0
-		        cat(c("Simulating avatar:", a, "for replicate:", r, "\n"))
-          print("hi-1")
-          print(r)
-          print(a)
-		        for (i in 1:numDaysToSimulate) {
-              print(paste("Dose: ", doses[i]))
-              print(paste("INRs: ", INRs[i]))
-		            pill = dose_cat(doses[i], maxDose)    # converts dose into closest pill form              
-		            testPatient = hamberg_2007(pill, Cs_super, avatars$AGE[a], as.character(avatars$CYP2C9[a]), as.character(avatars$VKORC1G[a]), 1, maxTime*numDaysToSimulate, seed)
-		            INRerror = INRerrorDist[i, r, a]
-		            measuredINR = round(testPatient$INRv[i*24+1] + INRerror, digits=1)  # round this to 1 decimal, add 1 b/c time is zero based
-              print(i)
-		            # keep track of the dose and INR values per person over time
-		            if (is.na(measuredINR)) {
-		                cat(c("Aw Snap...INR is NA:", "avatar:", avatarID, "day:", i, "\n"))
-		                print(testPatient$parameters)
-		                doses[i] = NA
-		                INRs[i] = NA
-		            } else {                
-		                Cs[((i-1)*24 + 1):Cs_rows, i] = testPatient$Cs[1:length(((i-1)*24 + 1):Cs_rows)]
-		                Cs_super = apply(Cs, 1, sum)[i*24+2]    
-                  
-		                INRs[i] = measuredINR
-		                print("hi")
-                  print(paste("INRs _ MeasuredINR: ", INRs[i]))
-		                # pick a protocol
-		                if (is.na(doses[min(i+1, numDaysToSimulate)])) {
-		                    INR.check[i] = i  # checked INR on this day (due to looping we are off by one day when we check INRs so 2,4,7 days are clinically 3,5,8 but don't worry it all works out)
-		                    if (avatars$Protocol[a] == "coumagen_pharm") {
-		                        doses = coumagen_pharm(measuredINR, doses, i, maxDose)
-		                    } else if (avatars$Protocol[a] == "coumagen_standard") {
-		                        doses = coumagen_standard(measuredINR, doses, i, maxDose)
-		                    } else if (avatars$Protocol[a] == "wilson") {
-		                        doses = wilson(measuredINR, doses, i, maxDose)		                    
-    		                } else if (avatars$Protocol[a] == "fixed_dose") {
-        		                doses = fixed_dose(doses, i) 
-        		            } else if (avatars$Protocol[a] == "wilson_coumagen_pharm") {
-        		                doses = wilson_coumagen_pharm(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "wilson_coumagen_standard") {
-        		                doses = wilson_coumagen_standard(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "cooper_intermt") {
-        		                doses = cooper_intermt(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "fen_intermt") {
-        		              doses = fen_intermt(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "rob_intermt") {
-        		              doses = rob_intermt(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "gedge_intermt") {
-        		              doses = gedge_intermt(measuredINR, doses, i, maxDose)
-        		            } else if (avatars$Protocol[a] == "ahc_clinical") {
-                        doses = ahc_clinical(as.numeric(as.character(measuredINR)), as.numeric(as.character(INRs[1:i])), as.numeric(as.character(INR.check[1:i])), as.numeric(as.character(doses)), i, maxDose, as.character(avatars$TINR[a]))
-                        print("hi2")
-                      } else if (avatars$Protocol[a] == "eu_pact_intermt") {#initial and alteration: eu_pact || maintenance: intermountain
-                        doses = eu_pact_intermt(measuredINR, doses, i, maxDose, avatars$AGE[a], as.character(avatars$VKORC1G[a]), avatars$BSA[a], avatars$TINR[a], as.character(avatars$AMI[a]), as.character(avatars$CYP2C9[a]), avatars$HEIGHT[a], avatars$WEIGHT[a])
-                     } else if (avatars$Protocol[a] == "eu_pact_ahc") {#initial and alteration: eu_pact || maintenance: ahc
-                        doses = eu_pact_ahc(measuredINR, doses, i, maxDose, avatars$AGE[a], as.character(avatars$VKORC1G[a]), avatars$BSA[a], avatars$TINR[a], as.character(avatars$AMI[a]), as.character(avatars$CYP2C9[a]), avatars$HEIGHT[a], avatars$WEIGHT[a], as.numeric(as.character(INRs[1:i])), as.numeric(as.character(INR.check[1:i])))
-                     } else {
-    		                    stop("Hey buddy, you forgot to add the protocol to the simulator - duh...")
-    		                }                
-			            }
-		            }
-		    }
+    # set simulations$replicates if undefined
+    if("replicates" %in% names(simulations) || is.null(simulations$replicates)) {
+      simulations$replicates = replicates
+    }
+    # pick up the random seed, otherwie fall back to default `initial_seed`
+    if(!is.null(simulations$seed)) {
+      initial_seed = simulations$seed
+    }
 
-		    Cs_super_out[, r] = apply(Cs, 1, sum)
-            print(paste("inr array: ", INRs))
-            print(paste("dose array: ", doses))
-            print(paste("check array: ", INR.check))
-            INR.array[, r, a] = INRs
-            dose.array[, r, a] = doses
-            INR.check.array[, r, a] = INR.check	
-            ID.array[, a] <- avatars$ID[a]
-		}		
-	}
- #return(avatars)
-	as.data.frame(list("INR"=INR.array, "Dose"=dose.array, "Check"=INR.check.array, "ID"= ID.array))
-  #list("INR"=INR.array, "Dose"=dose.array, "Check"=INR.check.array, "ID"= ID.array)
+    # delete random seed
+    simulations[which(names(simulations) %in% c("seed"))] <- NULL
+
+    # create a numbered list of simulations  
+    simulations = rep(list(simulations), nrow(avatars))
+  }
+  
+  if(length(simulations) != nrow(avatars)) {
+    stop("simulation param of incorrect length")
+  }
+
+  # assign random seeds to simulations
+
+  # set using the default seed
+  set.seed(initial_seed)
+
+  for(simulation in simulations) {
+
+    # set the seeds if undefined 
+    if("seed" %in% names(simulation) || is.null(simulation$seed)) {
+      simulation$seed = sample(10^12, simulation$replicates, replace=T)
+    }
+  }
+
+  # bind simulation params to avatar
+  simulation_in = sapply(1:nrow(avatars), function(n) {
+    list(list( avatar     = as.list(avatars[n,])
+        , simulation = simulations[[n]]   
+        ))
+  })
+
+  # in parallel conduct the simulation!
+  llply(simulation_in, process_avatar, .parallel = threads > 1)
 }
 
 
 
+process_avatar = function(simulation_in) {
+  # simulation_in contains
+  # (1) avatar as a list
+  # (2) simulation as a list
+  
+  # print(simulation_in)
+  attach(simulation_in, warn.conflicts = F)
 
 
+  # find the initial dose
+  avatar$InitialDose = initial_dose( as.data.frame(avatar)
+                                   , simulation$initial_dose)$InitialDose
+  # run sim for each replicate
+  sim_out = lapply(1:simulation$replicates, function(rep) {
+    set.seed(simulation$seed[rep])
+    inr       = array(NA, simulation$days)
+    inr_check = array(0, simulation$days)
+    dose      = array(NA, simulation$days)
+    # keep track of the dose superpositioning for each avatar
+    # Cs_super_out = array(0, dim=c(simulation$max_time*simulation$days+1
+    #                    , num_replicates)) 
+    dose[1] = avatar$InitialDose
+    Cs_rows = simulation$max_time * simulation$days+1
+    Cs = matrix(0, nrow = Cs_rows, ncol=simulation$days)
+    Cs_super = 0
 
+    inr_errors = rnorm(simulation$days, 0, 0.1)
 
+    for(day in 1:simulation$days) {
+      pill = dose_cat(dose[day], simulation$max_dose)
+      test_patient = hamberg_2007( pill
+                                 , Cs_super
+                                 , avatar$AGE
+                                 , as.character(avatar$CYP2C9)
+                                 , as.character(avatar$VKORC1G)
+                                 , 1
+                                 , simulation$max_time * simulation$days
+                                 , avatar$seed )
 
+      inr_error = inr_errors[day] # rnorm(1,0,0.1)
+      measured_inr = round(test_patient$INRv[day*24+1] + inr_error, digits=1)
 
+      if(is.na(measured_inr)) {
+        cat(c("aww snap"))
+        print(test_patient$paramaters)
+        dose[day] = NA
+        inr[day]  = NA
+      } else {
+        Cs[((day-1)*24 + 1):Cs_rows, day] = test_patient$Cs[1:length(((day-1)*24 + 1 ):Cs_rows)]
+        Cs_super = apply(Cs, 1, sum)[day*24+2] 
+        inr[day] = measured_inr
 
+        if (is.na(dose[min(day+1, simulation$days)])) {
+            inr_check[day] = day  # checked INR on this day (due to looping we are off by one day when we check inr so 2,4,7 days are clinically 3,5,8 but don't worry it all works out)
+            if (simulation$protocol == "coumagen_pharm") {
+                dose = coumagen_pharm(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "coumagen_standard") {
+                dose = coumagen_standard(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "wilson") {
+                dose = wilson(measured_inr, dose, day, simulation$max_dose)		                    
+            } else if (simulation$protocol == "fixed_dose") {
+                dose = fixed_dose(dose, day) 
+            } else if (simulation$protocol == "wilson_coumagen_pharm") {
+                dose = wilson_coumagen_pharm(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "wilson_coumagen_standard") {
+                dose = wilson_coumagen_standard(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "cooper_intermt") {
+                dose = cooper_intermt(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "fen_intermt") {
+              dose = fen_intermt(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "rob_intermt") {
+              dose = rob_intermt(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "gedge_intermt") {
+              dose = gedge_intermt(measured_inr, dose, day, simulation$max_dose)
+            } else if (simulation$protocol == "ahc_clinical") {
+            dose = ahc_clinical(as.numeric(as.character(measured_inr)), as.numeric(as.character(inr[1:day])), as.numeric(as.character(inr_check[1:day])), as.numeric(as.character(dose)), day, simulation$max_dose, as.character(avatar$TINR))
+            print("hi2")
+          } else if (simulation$protocol == "eu_pact_intermt") {#initial and alteration: eu_pact || maintenance: intermountain
+            dose = eu_pact_intermt(measured_inr, dose, day, simulation$max_dose, avatars$AGE, as.character(avatar$VKORC1G), avatar$BSA, avatar$TINR, as.character(avatar$AMI), as.character(avatar$CYP2C9), avatar$HEIGHT, avatar$WEIGHT)
+         } else if (simulation$protocol == "eu_pact_ahc") {#initial and alteration: eu_pact || maintenance: ahc
+            dose = eu_pact_ahc(measured_inr, dose, day, simulation$max_dose, avatar$AGE, as.character(avatar$VKORC1G), avatar$BSA, avatar$TINR, as.character(avatar$AMI), as.character(avatar$CYP2C9), avatar$HEIGHT, avatar$WEIGHT, as.numeric(as.character(inr[1:day])), as.numeric(as.character(inr_check[1:day])))
+         } else {
+                stop("Hey buddy, you forgot to add the protocol to the simulator - duh...")
+        }
+      }
+    }
+  }
+  list("INR" = inr, "Dose" = dose, "Check" = inr_check)
+})
+
+list( avatar     = avatar
+      , simulation = simulation
+      , sim_out    = sim_out
+      )
+}
+
+###### Note: max_time arguement is defined based on the adopted PK/PD model. So, since Hamberg PK/PD
+###### model is based on a 24-hr time period, we decided to make argument "max_time" constant "24".
+###### So, the regular user of the simulator does not need to change it when calling function "processAvatar".
