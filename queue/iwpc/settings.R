34aac93629695a89593212724eae355096d12547
Marcus Walz <mewalz@uwm.edu>
2015-03-16 13:13:19 -0500

    parallel execution working

---
+++ queue/iwpc/settings.R
@@ -0,0 +1,8 @@
+sim_settings = list ( protocol = "ahc_clinical"
+                    , initial ="pginitial_IWPC"
+                    , maxDose=15
+                    , days=500
+                    , rseed = 4321
+                    , numReplicates = 1
+                    , per_job = 300
+                    )
